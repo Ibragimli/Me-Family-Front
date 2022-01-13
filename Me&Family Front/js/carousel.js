e4c291dc1149e891df777eaaf2e520249ceea5fe
Ibragimli <elnur204@gmail.com>
2022-01-14 02:33:46 +0400

    Completed

---
+++ Me&Family Front/js/carousel.js
@@ -0,0 +1,20 @@
+
+
+$(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 15,
+        dotsEach: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+})
